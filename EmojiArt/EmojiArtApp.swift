90fd6cb3ede95030f3881ff0a3b18a4b5ebd7e1d
Georgi Markov <markov.georgi@gmail.com>
2023-02-11 15:01:23 -0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Georgi Markov on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
